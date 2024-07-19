648cccada7eafacb375626140aa66eecdcf2bf30
saranthecoder <115061220+saranthecoder@users.noreply.github.com>
2024-07-19 14:58:47 +0530

    pod

--- src/models/userLogin.model.js
+++ src/models/userLogin.model.js
@@ -1,30 +1,34 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    batchnumber: {
-        type: Number,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  batchnumber: {
+    type: Number,
+    required: true
+  },
+  POD: {
+    type: Number,
+    default: 0 // Initialize with 0
+  }
 }, { timestamps: true });
 
 // Function to get the model for a specific batch
 const getUserModelForBatch = (batchNumber) => {
-    const collectionName = `users_batch_${batchNumber}`;
-    return mongoose.model(collectionName, UserSchema, collectionName);
+  const collectionName = `FSD_B${batchNumber}`;
+  return mongoose.model(collectionName, UserSchema, collectionName);
 };
 
 export default getUserModelForBatch;
